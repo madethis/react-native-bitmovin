2c3213bbe67398bf8025c2e2932f66f745b15d26
Thomas B Homburg <thomas@madethis.dk>
2022-05-16 21:43:39 +0200

    prettier etc

--- examples/BasicPlayerTV/App.tsx
+++ examples/BasicPlayerTV/App.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import {View} from 'react-native';
+import React from "react";
+import { View } from "react-native";
 
-import 'react-native/tvos-types.d';
+import "react-native/tvos-types.d";
 
-import {BitmovinVideo, BitmovinVideoProvider} from 'react-native-bitmovin';
+import { BitmovinVideo, BitmovinVideoProvider } from "react-native-bitmovin";
 
-import {BITMOVIN_LICENSE_KEY} from './bitmovin-license-key';
+import { BITMOVIN_LICENSE_KEY } from "./bitmovin-license-key";
 
-const videos: {title: string; url: string}[] = [
+const videos: { title: string; url: string }[] = [
   {
-    title: 'Sintel',
-    url: 'https://bitmovin-a.akamaihd.net/content/sintel/hls/playlist.m3u8',
+    title: "Sintel",
+    url: "https://bitmovin-a.akamaihd.net/content/sintel/hls/playlist.m3u8",
   },
   {
-    title: 'Art of motion',
-    url: 'https://bitmovin-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8',
+    title: "Art of motion",
+    url: "https://bitmovin-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8",
   },
 ];
 
@@ -25,9 +25,9 @@ const App = () => {
         <BitmovinVideo
           source={videos[0].url}
           style={{
-            height: '100%',
-            width: '100%',
-            backgroundColor: '#000000',
+            height: "100%",
+            width: "100%",
+            backgroundColor: "#000000",
           }}
         />
       </View>
