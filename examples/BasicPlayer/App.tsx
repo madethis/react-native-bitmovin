1433329c8e522b2a3b1910ff56c687b858494575
Rasmus Styrk <styrken@gmail.com>
2022-06-15 10:35:27 +0200

    Added hls

--- examples/BasicPlayer/App.tsx
+++ examples/BasicPlayer/App.tsx
@@ -20,10 +20,12 @@ const videos: (BitmovinVideoProps["source"] & { title: string })[] = [
   {
     title: "Art of motion",
     dash: "https://bitdash-a.akamaihd.net/content/MI201109210084_1/mpds/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.mpd",
+    hls: "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8"
   },
   {
     title: "Sintel",
     dash: "https://bitdash-a.akamaihd.net/content/sintel/sintel.mpd",
+    hls: "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8"
   },
   {
     title: "Bad video",
--- tools/logger.swift
+++
@@ -1,8 +0,0 @@
-//
-//  logger.swift
-//  react-native-bitmovin
-//
-//  Created by Rasmus Styrk on 15/06/2022.
-//
-
-import Foundation
