d99b252f45f0966802634d90c48baf53026ea820
Rasmus Styrk <styrken@gmail.com>
2022-06-16 11:22:03 +0200

    Added events

--- ios/player/BitmovinVideo+events.swift
+++ ios/player/BitmovinVideo+events.swift
@@ -14,11 +14,77 @@ import BitmovinPlayer
  * This extension handles dispatching events from the bitmovin player back to react native
  */
 extension BitmovinVideo: PlayerListener {
+    
     func onEvent(_ event: Event, player _: Player) {
-        dump(event, name: "[Player Event]", maxDepth: 2)
-        
-        if let timeChanged = event as? TimeChangedEvent {
-            self.onTimeChanged?(["currentTime": timeChanged.currentTime])
+        switch(event) {
+        case is ReadyEvent:
+            self.onReady?(["type": "ready"])
+        case is PlayEvent:
+            self.onReady?(["type": "play"])
+        case is PlayingEvent:
+            self.onPlaying?(["type": "playing"])
+        case is PausedEvent:
+            self.onPaused?(["type": "paused"])
+        case is SeekEvent:
+            self.onSeek?(["type": "seek"])
+        case is SeekedEvent:
+            self.onSeeked?(["type": "seeked"])
+        case is TimeShiftEvent:
+            self.onTimeShift?(["type": "timeshift"])
+        case is TimeShiftedEvent:
+            self.onTimeShifted?(["type": "timeshifted"])
+        case is MutedEvent:
+            self.onMuted?(["type": "muted"])
+        case is UnmutedEvent:
+            self.onUnmuted?(["type": "unmuted"])
+        case is VideoSizeChangedEvent: //PlayerResized?
+            self.onPlayerResized?(["type": "playerresized"])
+        case is PlaybackFinishedEvent:
+            self.onPlaybackFinished?(["type": "playbackfinished"])
+        case let error as PlayerErrorEvent:
+            self.onError?(["type": "error", "code": error.code, "message": error.message])
+        case let warning as PlayerWarningEvent:
+            self.onWarning?(["type": "warning", "code": warning.code, "message": warning.message])
+        case is StallStartedEvent:
+            self.onStallStarted?(["type": "stallstarted"])
+        case is StallEndedEvent:
+            self.onStallEnded?(["type": "stallended"])
+        case let timeChanged as TimeChangedEvent:
+            self.onTimeChanged?(["time": timeChanged.currentTime, "type": "timechanged"])
+        case is CastAvailableEvent:
+            self.onCastAvailable?(["type": "castavailable"])
+        case is CastStoppedEvent:
+            self.onCastStopped?(["type": "caststopped"])
+        case is CastStartEvent:
+            self.onCastStart?(["type": "caststart"])
+        case is CastStartedEvent:
+            self.onCastStarted?(["type": "caststarted"])
+        case is CastWaitingForDeviceEvent:
+            self.onCastWaitingForDevice?(["type": "castwaitingfordevicee"])
+        case is SourceLoadedEvent:
+            self.onSourceLoaded?(["type": "sourceloaded"])
+        case is SourceUnloadEvent:
+            self.onSourceUnloaded?(["type": "sourceunloaded"])
+        case is AirPlayAvailableEvent:
+            self.onAirplayAvailable?(["type": "airplayavailable"])
+        case is AirPlayChangedEvent:
+            self.onAirplayChanged?(["type": "airplaychanged"])
+        case is DestroyEvent:
+            self.onDestroy?(["type": "destroy"])
+        case is DurationChangedEvent:
+            self.onDurationChanged?(["type": "durationchanged"])
+        case is PictureInPictureEnterEvent:
+            self.onPictureInPictureEnter?(["type": "pictureinpictureenter"])
+        case is PictureInPictureExitEvent:
+            self.onPictureInPictureEnter?(["type": "pictureinpictureexit"])
+        case is ControlsHideEvent:
+            self.onControlsHide?(["type": "controlshide"])
+        case is ControlsShowEvent:
+            self.onControlsShow?(["type": "controlsshow"])
+        case let sourceError as SourceErrorEvent:
+            self.onSourceError?(["type": "sourceerror", "code": sourceError.code, "message": sourceError.message])
+        default:
+            log("Unhandled event: \(event.name)")
         }
     }
 }
--- ios/player/BitmovinVideo.swift
+++ ios/player/BitmovinVideo.swift
@@ -45,9 +45,78 @@ class BitmovinVideo: UIView {
     }
     
     /**
-     *  Function to call when time has changed
+     *  All events that is emitted fromt he player
      */
+    @objc var onReady: RCTBubblingEventBlock?
+    @objc var onPlay: RCTBubblingEventBlock?
+    @objc var onPlaying: RCTBubblingEventBlock?
+    @objc var onPaused: RCTBubblingEventBlock?
+    @objc var onSeek: RCTBubblingEventBlock?
+    @objc var onSeeked: RCTBubblingEventBlock?
+    @objc var onTimeShift: RCTBubblingEventBlock?
+    @objc var onTimeShifted: RCTBubblingEventBlock?
+    @objc var onVolumeChanged: RCTBubblingEventBlock?
+    @objc var onMuted: RCTBubblingEventBlock?
+    @objc var onUnmuted: RCTBubblingEventBlock?
+    @objc var onPlayerResized: RCTBubblingEventBlock?
+    @objc var onPlaybackFinished: RCTBubblingEventBlock?
+    @objc var onError: RCTDirectEventBlock?
+    @objc var onWarning: RCTBubblingEventBlock?
+    @objc var onStallStarted: RCTBubblingEventBlock?
+    @objc var onStallEnded: RCTBubblingEventBlock?
+    @objc var onAudioChanged: RCTBubblingEventBlock?
+    @objc var onAudioAdded: RCTBubblingEventBlock?
+    @objc var onAudioRemoved: RCTBubblingEventBlock?
+    @objc var onVideoQualityChanged: RCTBubblingEventBlock?
+    @objc var onAudioQualityChanged: RCTBubblingEventBlock?
+    @objc var onVideoDownloadQualityChange: RCTBubblingEventBlock?
+    @objc var onAudioDownloadQualityChange: RCTBubblingEventBlock?
+    @objc var onVideoDownloadQualityChanged: RCTBubblingEventBlock?
+    @objc var onAudioDownloadQualityChanged: RCTBubblingEventBlock?
+    @objc var onVideoPlaybackQualityChanged: RCTBubblingEventBlock?
+    @objc var onAudioPlaybackQualityChanged: RCTBubblingEventBlock?
     @objc var onTimeChanged: RCTBubblingEventBlock?
+    @objc var onSegmentPlayback: RCTBubblingEventBlock?
+    @objc var onMetadata: RCTBubblingEventBlock?
+    @objc var onMetadataParsed: RCTBubblingEventBlock?
+    @objc var onMetadataChanged: RCTBubblingEventBlock?
+    @objc var onVideoAdaptation: RCTBubblingEventBlock?
+    @objc var onAudioAdaptation: RCTBubblingEventBlock?
+    @objc var onDownloadFinished: RCTBubblingEventBlock?
+    @objc var onSegmentRequestFinished: RCTBubblingEventBlock?
+    @objc var onCastAvailable: RCTBubblingEventBlock?
+    @objc var onCastStopped: RCTBubblingEventBlock?
+    @objc var onCastStart: RCTBubblingEventBlock?
+    @objc var onCastStarted: RCTBubblingEventBlock?
+    @objc var onCastWaitingForDevice: RCTBubblingEventBlock?
+    @objc var onSourceLoaded: RCTBubblingEventBlock?
+    @objc var onSourceUnloaded: RCTBubblingEventBlock?
+    @objc var onAirplayAvailable: RCTBubblingEventBlock?
+    @objc var onAirplayChanged: RCTBubblingEventBlock?
+    @objc var onDestroy: RCTBubblingEventBlock?
+    @objc var onPlaybackSpeedChanged: RCTBubblingEventBlock?
+    @objc var onDurationChanged: RCTBubblingEventBlock?
+    @objc var onViewModeChanged: RCTBubblingEventBlock?
+    @objc var onModuleReady: RCTBubblingEventBlock?
+    @objc var onSubtitleEnable: RCTBubblingEventBlock?
+    @objc var onSubtitleEnabled: RCTBubblingEventBlock?
+    @objc var onSubtitleDisable: RCTBubblingEventBlock?
+    @objc var onSubtitleDisabled: RCTBubblingEventBlock?
+    @objc var onVideoQualityAdded: RCTBubblingEventBlock?
+    @objc var onVideoQualityRemoved: RCTBubblingEventBlock?
+    @objc var onAudioQualityAdded: RCTBubblingEventBlock?
+    @objc var onAudioQualityRemoved: RCTBubblingEventBlock?
+    @objc var onTargetLatencyChanged: RCTBubblingEventBlock?
+    @objc var onLatencyModeChanged: RCTBubblingEventBlock?
+    @objc var onLicenseValidated: RCTBubblingEventBlock?
+    @objc var onDrmLicenseAdded: RCTBubblingEventBlock?
+    @objc var onAspectRatioChanged: RCTBubblingEventBlock?
+    @objc var onPictureInPictureAvailabilityChanged: RCTBubblingEventBlock?
+    @objc var onPictureInPictureEnter: RCTBubblingEventBlock?
+    @objc var onPictureInPictureExit: RCTBubblingEventBlock?
+    @objc var onControlsHide: RCTBubblingEventBlock?
+    @objc var onControlsShow: RCTBubblingEventBlock?
+    @objc var onSourceError: RCTBubblingEventBlock?
     
     /**
      * De-nit
